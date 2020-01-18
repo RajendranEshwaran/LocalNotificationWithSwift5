f532f14a6302675f9879868bf6074d416646568c
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-01-18 15:36:31 -0500

    Initial Commit

---
+++ LocalNotificationWithSwift5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNotificationWithSwift5
+//
+//  Created by gomathi saminathan on 1/18/20.
+//  Copyright © 2020 Rajendran Eshwaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
